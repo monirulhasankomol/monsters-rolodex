9e88446a612da4f04f9cf9f660e3f3e57960e66f
Monirul Hasan <monirulhasan0500@gmail.com>
2020-02-28 18:29:41 +0600

    Updates

---
+++ precache-manifest.ce890d81e5217f6ec73f73c11afb2917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f9c19cc34ab7235e38354501e3ba1f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3213fe8e116aa80b5ee1",
+    "url": "/monsters-rolodex/static/css/main.3cda2ebb.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3213fe8e116aa80b5ee1",
+    "url": "/monsters-rolodex/static/js/main.8f077167.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
