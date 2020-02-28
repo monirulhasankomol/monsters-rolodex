99459e76a71537ae4fc4982c18b0e74cbcae2c64
Monirul Hasan <monirulhasan0500@gmail.com>
2020-02-28 18:32:40 +0600

    Adding files for github pages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -10,3 +11,35 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+/*
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.handleclick2 = this.handleclick1.bind(this)
+  }
+  handleclick1() {
+    console.log('button 1 clicked');
+  }
+
+  handleclick3 = () => { console.log('button 3 clicked'); }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleclick1()}>Click 1</button>
+        <button onClick={this.handleclick1}>Click 2</button>
+        <button onClick={this.handleclick2}>Click 3</button>
+        <button onClick={this.handleclick3}>Click 4</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+*/
